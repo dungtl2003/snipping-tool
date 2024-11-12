60492809c9898a91566af17220ac34d78f7d914b
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-11-12 19:33:53 +0700

    feat: add observer pattern for mouse events

--- components/snipper_window.py
+++ components/snipper_window.py
@@ -1,3 +1,4 @@
+from typing import Callable, List, Tuple
 from PyQt6.QtWidgets import (
     QFileDialog,
     QMainWindow,
@@ -5,6 +6,7 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+from components.mouse_observer import MouseObserver
 
 
 class SnipperWindow(QMainWindow):
@@ -22,6 +24,10 @@ class SnipperWindow(QMainWindow):
         self.label = ImageLabel()
         self.label.setVisible(True)
 
+        self.color_picker = ColorPicker(self)
+        self.btn_pick_color = QPushButton("Pick Color")
+        self.btn_pick_color.clicked.connect(self.toggle_color_picker)
+
         self.btn_capture = QPushButton("Capture")
         self.btn_capture.clicked.connect(self.capture)
 
@@ -30,13 +36,42 @@ class SnipperWindow(QMainWindow):
         self.btn_save.setVisible(False)
 
         layout.addWidget(self.btn_capture)
+        layout.addWidget(self.btn_pick_color)
         layout.addWidget(self.label)
         layout.addWidget(self.btn_save)
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+    def subscribers(
+        self,
+    ) -> List[Tuple[Callable[..., None], MouseObserver.SubscribeEvent]]:
+        """
+        Return the subscribers.
+        :return: the subscribers
+        :rtype: List[Tuple[Callable[..., None], MouseObserver.SubscribeEvent]]
+        """
+        return [
+            (self.color_picker.pick_color, MouseObserver.SubscribeEvent.PRESSED),
+            (
+                self.color_picker.handle_mouse_movement,
+                MouseObserver.SubscribeEvent.MOVED,
+            ),
+        ]
+
+    def toggle_color_picker(self) -> None:
+        """
+        Toggle the color picker.
+        :return: None
+        """
+        self.color_picker.toggle()
+
     def capture(self) -> None:
+        """
+        Capture the screen.
+        Note that when capturing, no other actions can be performed.
+        :return: None
+        """
         self.capturer = Capture(self)
         self.btn_save.setVisible(True)
 
@@ -51,5 +86,6 @@ class SnipperWindow(QMainWindow):
             image.save(file_name)
 
 
+from components.color_picker import ColorPicker
 from components.capture import Capture
 from components.image_label import ImageLabel
